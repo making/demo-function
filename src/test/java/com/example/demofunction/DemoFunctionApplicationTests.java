b9bcaaff42212f5ff2926c5ef96a1e9e88c9d0e3
Toshiaki Maki <tmaki@pivotal.io>
2018-02-04 21:26:14 +0900

    Add test

--- src/test/java/com/example/demofunction/DemoFunctionApplicationTests.java
+++ src/test/java/com/example/demofunction/DemoFunctionApplicationTests.java
@@ -2,15 +2,23 @@ package com.example.demofunction;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DemoFunctionApplicationTests {
+import static org.assertj.core.api.Assertions.assertThat;
 
-	@Test
-	public void contextLoads() {
-	}
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DemoFunctionApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    public void contextLoads() {
+        String res = restTemplate.postForObject("/greeter", "World", String.class);
+        assertThat(res).isEqualTo("Hello World");
+    }
 
 }
